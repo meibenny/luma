409792d9ae78b37fb750ee4b2a6be05fb504c02f
Benny Mei <meibenny@gmail.com>
2021-07-21 01:21:14 -0400

    close file after reading

--- video_tools.py
+++ video_tools.py
@@ -1,15 +1,8 @@
 import cv2
 
 def grab_first_frame(filename):
-    print(filename)
-    #cap = cv2.VideoCapture(filename)
-    #_, frame = cap.read()
-    #_, frame = cap.read()
-    #b,g,r = cv2.split(frame)
-    #recolored_frame = cv2.merge((r,g,b))
-
     cap = cv2.VideoCapture(filename)
     _, frame = cap.read()
     cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-
+    cap.release()
     return cv2image
